19cc79ad2de511e2f218f6dff99f59ad29791534
Adrian Guillermo <adridevega@gmail.com>
2020-11-10 21:59:00 +0000

    correcion de IMC

--- Cip/src/EjerciciosCip4/Aleatorio.java
+++ Cip/src/EjerciciosCip4/Aleatorio.java
@@ -3,7 +3,7 @@ package EjerciciosCip4;
 public class Aleatorio {
 
 	public static void main(String[] args) {
-		  System.out.println(String.format("%.0f", Math.random()*10));
+		  System.out.println(String.format("%.0f", Math.random()*11));
 		
 	}
 
--- Cip/src/EjerciciosCip4/IMC.java
+++ Cip/src/EjerciciosCip4/IMC.java
@@ -2,24 +2,34 @@ package EjerciciosCip4;
 
 import javax.swing.JOptionPane;
 
-public class IMC {
+public class IMC 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		double peso ,IMC;
 		double altura;
 		peso= Double.parseDouble(JOptionPane.showInputDialog("Introduzca el peso en kilogramos"));
 		altura= Double.parseDouble(JOptionPane.showInputDialog("Introduzca la altura en metros"));
 		IMC = peso / Math.pow(altura, 2) ;
-		System.out.println(IMC);
-			if(IMC>=18 || IMC<=25) {
+		System.out.println(String.format("%.2f", IMC));
+		if(IMC>=18 && IMC<=25) 
+			{
 			System.out.println("Estas saludable");
 			}
-			if(IMC<18) {
-				System.out.println("Estas por debajo de tu IMC saludable");
+			else 
+			{
+				if(IMC<18) 
+				{
+					System.out.println("Estas por debajo de tu IMC saludable");
+				}
+				else if(IMC>25) 
+				{
+					System.out.println("Estas por encima de tu IMC saludable");
+				}	
 			}
-			if(IMC>25) {
-				System.out.println("Estas por encima de tu IMC saludable");
-			}	
-	}
 
+		}
+	
 }
+
